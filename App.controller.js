bf5fdb3a7fe06f1e5289951f08e511d267b5d1f7
Mister Gillouz <34444967+Mistergillouz@users.noreply.github.com>
2019-01-18 15:58:36 +0100

    Update App.controller.js

--- App.controller.js
+++ App.controller.js
@@ -126,19 +126,158 @@ sap.ui.define([
   C.prototype.onRequestContextMenu = function (oEvent) {
     const item = oEvent.getParameter('listItem').getBindingContext().getObject()
     const done = oEvent.getParameter('done')
+    const menuItems = []
 
-    const menu = new sap.m.Menu({
+    switch (item.nodeType) {
+      case 'Reference':
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Edit Properties',
+          icon: 'sap-icon://edit',
+          press: () => this._onEditReference(item)
+        }))
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Show referenced cell',
+          press: () => this._onShowReferenceCell(item)
+        }))
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Delete...',
+          icon: 'sap-icon://delete',
+          startsSection: true,
+          press: () => this._onDeleteReference(item)
+        }))
+        break
+
+      case 'VariableFolder':
+        menuItems.push(new sap.m.MenuItem({
+          icon: 'sap-icon://create',
+          text: 'Create Variable...',
+          press: () => this._onCreateVariable(item)
+        }))
+        break
+
+      case 'Variable':
+        menuItems.push(new sap.m.MenuItem({
+          icon: 'sap-icon://edit',
+          text: 'Edit...',
+          press: () => this._onEditVariable(item)
+        }))
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Rename...',
+          press: () => this._onRenameVariable(item)
+        }))
+
+        if (item['@qualification'] !== 'Measure') {
+          this._appendGeoMenu(menuItems, item, true)
+        }
+
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Duplicate',
+          icon: 'sap-icon://duplicate',
+          startsSection: true,
+          press: () => this._onDuplicateVariable(item)
+        }))
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Delete...',
+          icon: 'sap-icon://delete',
+          startsSection: true,
+          press: () => this._onDeleteVariable(item)
+        }))
+        break
+
+      case 'TimeDimension':
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Create Time Dimension...',
+          icon: 'sap-icon://create-entry-time',
+          press: () => this._onCreateTimeDimension(item)
+        }))
+        break
+
+      case 'TimeLevel':
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Edit Time Dimension...',
+          icon: 'sap-icon://edit',
+          press: () => this._onEditTimeDimension(item)
+        }))
+
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Delete Time Dimension...',
+          icon: 'sap-icon://delete',
+          press: () => this._onDeleteTimeDimension(item)
+        }))
+        break
+
+      case 'Link':
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Unmerge...',
+          icon: 'sap-icon://broken-link',
+          press: () => this._onUnMerge(item)
+        }))
+
+        menuItems.push(new sap.m.MenuItem({
+          text: 'Edit Properties...',
+          icon: 'sap-icon://edit',
+          press: () => this._onEditLinkProperties(item)
+        }))
+
+        this._appendGeoMenu(menuItems, item, true)
+        break
+        // Fallthru
+
+      case 'Geo':
+        this._appendGeoMenu(menuItems, item, false)
+        break
+
+      case 'Object':
+        const qualification = item['@qualification']
+        if (qualification === 'Measure') {
+          menuItems.push(new sap.m.MenuItem({
+            text: 'Change Type',
+            items: [
+              new sap.m.MenuItem({
+                text: 'Number',
+                press: () => this._onChangeTypeNumber(item)
+              }),
+              new sap.m.MenuItem({
+                text: 'Decimal',
+                press: () => this._onChangeTypeDecimal(item)
+              })
+            ]
+          }))
+        } else {
+          this._appendGeoMenu(menuItems, item, false)
+        }
+        break
+    }
+
+    if (menuItems.length) {
+      const menu = new sap.m.Menu({ items: menuItems })
+      done(menu)
+    }
+  }
+
+  C.prototype._appendGeoMenu = function (menuItems, item, addSeparator) {
+    menuItems.push(new sap.m.MenuItem({
+      icon: 'sap-icon://world',
+      text: 'Edit as Geography',
+      startsSection: addSeparator,
       items: [
         new sap.m.MenuItem({
-          text: "item# " + item.displayName + ' type: ' + item.nodeType,
-          press: () => alert('Pressed!')
+          text: 'By Name...',
+          press: () => this._onEditAsGeoByName(item)
         }),
-        new sap.m.MenuItem({text: "Les Gens"})
+        new sap.m.MenuItem({
+          text: 'By Latitude / Longitude...',
+          press: () => this._onEditAsGeoByLongLat(item)
+        })
       ]
-    })
-  
-    done(menu)
+    }))
+
+    menuItems.push(new sap.m.MenuItem({
+      text: 'Reset...',
+      enabled: item.nodeType === 'Geo',
+      press: () => this._onReset(item)
+    }))
   }
 
   return C
-});
+})
